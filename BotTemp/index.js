1b8c3cdb0fe8af71b67ee85be0d1767400f17711
GMCgit <goranivancic117@gmail.com>
2020-08-03 21:38:26 +0200

    Start of blackjack bot

--- BotTemp/index.js
+++ BotTemp/index.js
@@ -2,12 +2,13 @@ const Discord = require('discord.js');
 const { prefix } = require('./config.json');
 const client = new Discord.Client();
 const fs= require('fs');
+const blackjack = require('./commands2/blackjack');
 
 client.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./commands2').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
+	const command = require(`./commands2/${file}`);
 
 	// set a new item in the Collection
 	// with the key as the command name and the value as the exported module
@@ -22,15 +23,19 @@ client.once('ready', () => {
 //TO DO LIST:
 //blackjack/poker
 
-
+//When there is a new message start something if starts with Prefix
 client.on('message', async message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
+
+	//Get args and command
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const command = args.shift().toLowerCase();
 	
 	if (!client.commands.has(command)) return;
 
+
+	//Run the file nammed "command"
 	try {
 		client.commands.get(command).execute(message, args);
 	} catch (error) {
@@ -39,4 +44,5 @@ client.on('message', async message => {
 	}
 });
 
-client.login(process.env.token);
+//Logins
+client.login();

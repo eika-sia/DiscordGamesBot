afa50ae1ad5755b70ebcc7efb4ddfc2225a8fb38
GMCgit <69117291+GMCgit@users.noreply.github.com>
2020-08-04 20:11:18 +0200

    Update test.js

--- cmds/test.js
+++ cmds/test.js
@@ -1,7 +1,7 @@
 module.exports.run = async (bot, msg, args, db) => {
     msg.channel.send('Success!');
 
-    if (msg.guild.me.hasPermission("MANAGE_ROLES")) {
+    if (msg.guild.me.hasPermission("MANAGE_SERVER")) {
         msg.channel.send('True!')
     }
 }

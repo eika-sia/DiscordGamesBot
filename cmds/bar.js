2c484b2b88ae89da03f0711270e04b337d036cb2
GMCgit <69117291+GMCgit@users.noreply.github.com>
2020-08-04 18:52:54 +0200

    Update bar.js

--- cmds/bar.js
+++ cmds/bar.js
@@ -1,5 +1,5 @@
-module.exports.run = async (bot, msg, args, db) => {
-		if (msg.member=='479346286730936330') {
+module.exports.run = async (bot, msg, args, db, UserID) => {
+		if (UserID=='479346286730936330') {
 			msg.channel.send('nub');
 		} else
 			msg.channel.send("Bar Rank!");

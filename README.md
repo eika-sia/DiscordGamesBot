37787f42db4a1eb1d7bf6a30d1f831e172e926bc
GMCgit <goranivancic117@gmail.com>
2020-08-04 15:50:00 +0200

    a

---
+++ README.md
@@ -0,0 +1,13 @@
+# My first bot
+
+Hello everyone, this is my github repository for a discord bot I'm making using discord.js. The bot does not have any real direction what it will have or what will it do but it's
+a combination of multiple online tutorials and my code. Each function is stored in it's javascript file so you can look at it. Command names are pretty straight foward, if you
+want the bot added it should work via link: [link](https://discord.com/api/oauth2/authorize?client_id=738693849051824160&permissions=1073741815&scope=bot). Bot will ask you for
+a lot of permissions but just unselect the ones you don't want, read and send messages should be enough for it. You can use this code to learn to develop your bot through
+making changes to my code. You can probably find better bots for any functions you see here but this one is open source and I think for an example of a simple bot.
+## It's current uses
+
+Currently it's most comlex task is creating gradients with formating for a rename command in a Minecraft server (mc-rebels.com if you want to join). It can respond with bar (a joke) and it can bulk delete messages with prune.
+
+
+Plans to make this bot are mostly unknown as I do stuff to it when I want or get a fun idea. Current project I'll be working on is blackjack singleplayer and than multiplayer. 
---
+++ index.js
@@ -0,0 +1,144 @@
+//To do list!
+//Fix files: all
+//test.js - done
+//setPrefix.js - done
+//validRole.js - done
+//index.js - done
+//New files ready.
+
+
+// require packages
+const Discord = require('discord.js');
+const fs = require('fs');
+require('dotenv/config');
+
+// initialise are bot
+const bot = new Discord.Client();
+bot.commands = new Discord.Collection();
+
+//import settings
+let prefix;
+const owner = process.env.OWNER;
+
+//initalise database (Firabase!)
+
+const Firebase = require('firebase/app');
+const FieldValue = require('firebase-admin/').firestore.FieldValue;
+const admin = require('firebase-admin');
+const serviceAccount = require('./ServiceAccount.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+let db = admin.firestore();
+
+
+//read commands files
+fs.readdir('./cmds', (err,files) => {
+    if (err) {
+        console.log(err);
+    }
+
+    let cmdFiles = files.filter(f => f.split(".").pop() === "js");
+
+    if (cmdFiles.length === 0){
+        console.log("No files found");
+        return;
+    }
+
+    cmdFiles.forEach((f,i) => {
+        let props = require(`./cmds/${f}`);
+        console.log(`${i+1}: ${f} loaded`);
+        bot.commands.set(props.help.name, props);
+    })
+})
+
+
+
+
+
+bot.on('ready', async () => {
+    console.log("Hello, im ready");
+
+});
+
+
+bot.on('message',msg => {
+    db.collection('guilds').doc(msg.guild.id).get().then((q) =>{
+        if(q.exists){
+            prefix=q.data().prefix
+        }
+    }).then(() =>{
+        if (msg.channel.type === "dm") return;
+        if (msg.author.bot) return;
+
+        let msg_array = msg.content.split(" ");
+        let command = msg_array[0];
+        let args = msg_array.slice(1);
+
+        if (!command.startsWith(prefix)) return;
+
+        if (bot.commands.get(command.slice(prefix.length))){
+            db.collection('guilds').doc(msg.guild.id).get().then((q) =>{
+                if (q.exists) {
+                    if(q.data().guildOwnerID === msg.author.id) {
+                        let cmd = bot.commands.get(command.slice(prefix.length));
+                        if (cmd) {
+                            cmd.run(bot, msg, args, db).then(() => {
+                                console.log('Owner used a commad!');
+                            });
+                        }
+                    } else {
+                        let allowed = false;
+                        
+                        db.collection('roles').doc(msg.guild.id).get().then((r) =>{
+                            for (var i = 0; i<r.data().role_id.length; i++) {
+                                msg.member.roles.role_id.forEach((uRole) => {
+                                    let tmp_role = String(uRole);
+                                    tmp_role= tmp_role.substring(3, tmp_role.length-1);
+                                    if (tmp_role === r.data.role_id[i]) {
+                                        let cmd = bot.commands.get(command.slice(prefix.length));
+                                        if (cmd) {
+                                            cmd.run(bot, msg, args, db).then(() =>{
+                                                allowed=true;
+                                                console.log("Valid user command!");
+                                            }).catch((err) => {
+                                                console.log(err);
+                                            })
+                                        }
+                                    }
+                            })
+                            }
+                        }).then(() => {
+                            if(!allowed) {
+                                msg.channel.send('You can\'t do that!')
+                            }
+                        })
+                    }
+                }
+            })
+
+        }  
+
+    })
+    
+});
+
+bot.on('guildCreate', async gData => {
+    db.collection('guilds').doc(gData.id).set({
+        'guildID': gData.id,
+        'guildName': gData.name,
+        'guildOwner': gData.owner.user.username,
+        'guildOwnerID':gData.owner.id,
+        'guildMemberCount': gData.memberCount,
+        'prefix': '&'
+    });
+
+    db.collection('roles').doc(gData.id).set({
+        role_id: []
+    })
+});
+
+// Bot login
+bot.login(process.env.token);

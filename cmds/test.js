76e3f3f07d3be1c196beeafa21c9c701b175afab
GMCgit <goranivancic117@gmail.com>
2020-08-04 19:26:39 +0200

    Update test.js

--- cmds/test.js
+++ cmds/test.js
@@ -1,6 +1,9 @@
 module.exports.run = async (bot, msg, args, db) => {
     msg.channel.send('Success!');
 
+    if (message.guild.me.hasPermission("MUTE_MEMBERS")) {
+        msg.channel.send('True!')
+    }
 }
 
 module.exports.help = {

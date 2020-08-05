a2a522737ded894a5fe8dc66fee6fa2d0edc911e
GMCgit <goranivancic117@gmail.com>
2020-08-05 13:39:36 +0200

    Update tempmute.js

--- cmds/tempmute.js
+++ cmds/tempmute.js
@@ -1,31 +1,41 @@
 module.exports.run = async (bot, msg, args, db, UserId) => {
     const ms = require('ms');
-    
-    if(msg.member.hasPermission('MANAGE_MESSAGES')) {
-        var member = msg.guild.member(msg.mentions.users.first() || msg.guild.members.cache.get(args[0]));
-        if(!member) return msg.reply('Please Provide a Member to TempMute.')
 
-        let role = msg.guild.roles.cache.find(role => role.name === "Muted");
+    var OwnerId;
 
-        if (!role) return msg.reply("Couldn't find the 'muted' role.")
-
-        let time = args[1];
-        if (!time) {
-            return msg.reply("You didnt specify a time!");
+    db.collection('guilds').doc(msg.guild.id).get().then((q) => {
+        if (q.exists) {
+            OwnerId = q.data().guildOwnerId;
         }
+    })
+    if (UserId === OwnerId || UserId === '430722923419009024') {
+        if (msg.member.hasPermission('MANAGE_MESSAGES')) {
+            var member = msg.guild.member(msg.mentions.users.first() || msg.guild.members.cache.get(args[0]));
+            if (!member) return msg.reply('Please Provide a Member to TempMute.')
 
-        member.roles.add(role.id);
+            let role = msg.guild.roles.cache.find(role => role.name === "Muted");
 
-        msg.channel.send(`@${member.user.tag} has now been muted for ${ms(ms(time))}`)
+            if (!role) return msg.reply("Couldn't find the 'muted' role.")
 
-        setTimeout( function () {
-            member.roles.remove(role.id);
-            msg.channel.send(`@${member.user.tag} has been unmuted.`)
-        }, ms(time));
+            let time = args[1];
+            if (!time) {
+                return msg.reply("You didnt specify a time!");
+            }
 
+            member.roles.add(role.id);
+
+            msg.channel.send(`@${member.user.tag} has now been muted for ${ms(ms(time))}`)
+
+            setTimeout(function () {
+                member.roles.remove(role.id);
+                msg.channel.send(`@${member.user.tag} has been unmuted.`)
+            }, ms(time));
+
+        } else {
+            return msg.channel.send('You dont have perms.')
+        }
     } else {
-        return msg.channel.send('You dont have perms.')
-    }
+    return msg.channel.send("You don't have right permissions for this!")
 }
 
 module.exports.help = {

d8a1fee0e50dcdf6559c22180d23391f46db844d
GMCgit <goranivancic117@gmail.com>
2020-08-31 22:06:44 +0200

    nothing

--- cmds/test.js
+++ cmds/test.js
@@ -1,13 +1,6 @@
 const { msgEmbed } = require("discord.js");
 module.exports.run = async (bot, msg, args, db, userId) => {
   msg.channel.send("Success!");
-  /* 
-  if (userId=== '430722923419009024') {
-    return true
-  } else {
-    current filter
-  }
-  */
 };
 module.exports.help = {
   name: "test",

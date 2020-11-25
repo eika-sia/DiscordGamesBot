818f5c2f3f2da8b1816763971dcd5b45868b5c57
GMCgit <goranivancic117@gmail.com>
2020-11-25 13:40:09 +0100

    small change

--- cmds/test.js
+++ cmds/test.js
@@ -1,6 +1,16 @@
 const { msgEmbed } = require("discord.js");
 module.exports.run = async (bot, msg, args, db, userId) => {
-  msg.channel.send("Success!");
+  let strings = ["Success!", "Success2!"];
+  let msgs = [];
+
+  for (i=0; i<strings.length; i++) {
+    let themMsg = await msg.channel.send(strings[i]);
+    msgs.push(themMsg);
+  }
+
+  for (j = 0; j<strings.length; j++) {
+    console.log(msgs[j].content === strings[j]);
+  }
 };
 module.exports.help = {
   name: "test",
